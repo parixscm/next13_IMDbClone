a59939ddcb374cb6d0d5c42165cf0a5caf3bb715
parixscm <parixscm@gmail.com>
2023-02-23 01:02:04 +0900

    💄 Design: CHANGE topmost layout page style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="max-w-6xl px-12 py-4 sm:px-24 sm:py-10 sm:mx-auto">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
