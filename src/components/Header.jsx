330bc1196e9f264b5f5fd08c168114af24f2faa9
parixscm <parixscm@gmail.com>
2023-02-23 00:59:43 +0900

    📝 Feat: ADD Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import MenuItem from "./MenuItem";
+import { AiFillHome, AiFillInfoCircle } from "react-icons/ai";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center">
+      <div className="flex space-x-8 sm:space-x-16">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+        <MenuItem title="ABOUT" address="/about" Icon={AiFillInfoCircle} />
+      </div>
+      <div className="">
+        <Link href="/">
+          <h2 className="text-2xl sm:space-x-2">
+            <span className="px-2 py-1 bg-amber-500 font-bold rounded-lg">
+              IMDb
+            </span>
+            <span className="hidden text-xl sm:inline">Clone</span>
+          </h2>
+        </Link>
+      </div>
+    </header>
+  );
+}
