eaf694e44d4df8bc62bad98f442b01427de692eb
parixscm <parixscm@gmail.com>
2023-02-23 14:59:17 +0900

    📝 Feat: ADD Navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Providers from "./Providers";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
       <body className="max-w-6xl min-h-screen px-12 py-4 transition-colors duration-300 text-gray-700 sm:px-24 sm:py-10 sm:mx-auto dark:bg-gray-700 dark:text-gray-200">
         <Providers>
           <Header />
+          <Navbar />
           {children}
         </Providers>
       </body>
