cfe06dbb057d50a585c7710c8540b1c98314ee61
parixscm <parixscm@gmail.com>
2023-02-23 14:53:08 +0900

    📝 Feat: ADD About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,22 @@
+export default function AboutPage() {
+  return (
+    <div className="space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        IMDb is the world most popular and authoritative source for information
+        on movies, TV shows and celebrities. Products and services to help fans
+        decide what to watch and where to watch it include: the IMDb website for
+        desktop and mobile devices; apps for iOS and Android; and X-Ray on Prime
+        Video.
+      </p>
+      <p>
+        IMDb also produces IMDb original video series and podcasts. For
+        entertainment industry professionals, IMDb provides IMDbPro and Box
+        Office Mojo. IMDb licenses information from its vast and authoritative
+        database to third-party businesses worldwide; learn more at
+        developer.imdb.com. IMDb is an Amazon company. For more information,
+        visit https://www.imdb.com/press and follow @IMDb.
+      </p>
+    </div>
+  );
+}
