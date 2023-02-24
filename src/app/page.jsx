d405ec16d310f2ba1002bff83e4a8464226f23c9
parixscm <parixscm@gmail.com>
2023-02-24 16:12:43 +0900

    ✨ Feat: ADD function fetching data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,17 @@
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
   return <h1>Home</h1>;
 }
