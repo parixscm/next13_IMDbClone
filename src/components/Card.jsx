f169fad723fd246fd9ea3b9106b00a3ad97d5bc8
parixscm <parixscm@gmail.com>
2023-02-26 21:05:12 +0900

    📝 Feat: ADD Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FiThumbsUp } from "react-icons/fi";
+
+export default function Card({ result }) {
+  console.log(result);
+  return (
+    <div className="group cursor-pointer rounded-lg transition-shadow duration-200 sm:p-3 sm:m-2 sm:hover:shadow-slate-400 sm:shadow-md sm:border sm:border-slate-300">
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          alt="movie image"
+          placeholder="blur"
+          blurDataURL="/spinner.svg"
+          style={{ maxWidth: "100%", height: "auto" }}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
+        />
+        <div className="p-2">
+          <p className="line-clamp-2">{result.overview}</p>
+          <h2 className="truncate text-lg font-bold">
+            {result.title || result.name}
+          </h2>
+          <p className="flex items-center text-sm">
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className="h-5 mr-1 ml-3" />
+            {result.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,10 @@
+import Card from "./Card";
+
 export default function Results({ results }) {
   return (
-    <div>
+    <div className="py-4 max-w-6xl mx-auto sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
       {results.map(result => (
-        <div key={result.id}>
-          {result.original_title || result.original_name}
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
